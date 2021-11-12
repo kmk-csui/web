5c7eea6d760d94496ab35881411b2cc00c773165
Papolonite <gabrielerwhian@gmail.com>
2021-11-12 07:03:42 +0700

    Revert "feat ; basic html structure"
    This reverts commit 50f6eef4c6e9b3230717fcf0880a7c0e876979e7.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HeaderComp from './components/header/HeaderComp'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HeaderComp />
   </React.StrictMode>,
   document.getElementById('root')
 );
