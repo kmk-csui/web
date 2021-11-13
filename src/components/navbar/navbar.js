084993455645f6f9934f1e466a1a72863eef1462
Antonius Anggito Arissaputro <antoniusanggito@gmail.com>
2021-11-13 23:58:13 +0700

    Add mobile responsive

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,29 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './navbar.css'
 import KMKLogo from './../../assets/KMK-logo-small.svg'
+import ArrowUp from './../../assets/arrow-up.svg'
 import PropTypes from 'prop-types'
 
 const Navbar = ({ navMenu }) => {
+  const [navToggled, setNavToggled] = useState(false)
+  const [windowDimension, setWindowDimension] = useState(null);
+
+  useEffect(() => {
+    setWindowDimension(window.innerWidth);
+  }, []);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimension(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const isMobile = windowDimension <= 640;
   const currentPage = window.location.pathname === "/" ? "home" : window.location.pathname.slice(1);
-  
+
+  const handleNavToggled = () => {
+    setNavToggled(!navToggled)
+  }
+
   return (
     <div className="nav-wrapper">
+      <button className="nav-mobile-icon" onClick={handleNavToggled}>
+        <img src={ArrowUp} alt="Navbar" />
+      </button>
       <div className="nav">
         <a href="/" className="nav-title">
           <img src={KMKLogo} className="nav-logo" alt="KMK-logo" />
-          KMK Fasilkom UI
+          <p>KMK Fasilkom UI</p>
         </a>
-        <div className="nav-right">
-          {navMenu.map((menu, key) => (
-              <a 
+        {(isMobile && navToggled) | (!isMobile) ?
+          <ul>
+            {navMenu.map((menu, key) => (
+              <li 
                 key={key} 
-                href={menu === "Home" ? "/" : `/${menu.toLowerCase()}`}
                 className="nav-item" 
-                style={menu.toLowerCase() === currentPage ? {fontWeight: "bold"} : {}}>
-                  {menu}
-              </a>  
-          ))}
-        </div>
+                style={!isMobile && menu.toLowerCase() === currentPage ? {fontWeight: "bold"} : {}}>
+                  <a href={menu === "Home" ? "/" : `/${menu.toLowerCase()}`}>
+                    {!isMobile ? menu : `> ${menu} <`}
+                  </a>
+              </li>  
+            ))}
+          </ul>
+          : null
+        }
       </div>
     </div>
   )
