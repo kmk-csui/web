c802fd471ac8f95dfc6a0f550c8f71e29c55317e
Christopher Bagas Laiputra <christopher.bagas@ui.ac.id>
2021-11-17 21:54:40 +0700

    footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Footer.css';
+
+function Footer() {
+    return(
+        <div className="Footer">
+            <div className="container">
+                Copyright © 2021 - KMK Fasilkom UI 2021
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
