2286811d9dfa2ef81fd55ee25e0ab9109fa13bde
Teofanus Gary <garyteofanus@gmail.com>
2021-12-05 04:16:47 +0700

    Add component with styling

--- src/components/filosofi/Filosofi.js
+++ src/components/filosofi/Filosofi.js
@@ -1,20 +1,26 @@
 import React from 'react'
+import logo from '../../assets/big-logo.png';
+import './Filosofi.css'
 
 export default function Filosofi() {
   return (
     <div>
       <h1 className="header">Filosofi Logo</h1>
       <div className="indent">
-        <img/>
-        <p>Salib merupakan salah satu elemen penting dari agama Katolik yang melambangkan pengorbanan Yesus Kristus
+        <img src={logo} alt="Big KMK Logo" height={3}/>
+        <p>
+          Salib merupakan salah satu elemen penting dari agama Katolik yang melambangkan pengorbanan Yesus Kristus
           untuk menebus dosa umat manusia. Salib yang dibuat dengan dasar sirkuit dimaksudkan untuk merepresentasikan
           keluarga mahasiswa katolik dari Fakultas Ilmu Komputer sebab salah satu elemen pembangun komputer adalah
-          sirkuit.</p>
+          sirkuit.
+        </p>
       </div>
-      <p>Merpati merupakan lambang dari turunnya Roh Kudus. Adanya merpati dalam logo KMK Fasilkom UI melambangkan
+      <p>
+        Merpati merupakan lambang dari turunnya Roh Kudus. Adanya merpati dalam logo KMK Fasilkom UI melambangkan
         harapan KMK Fasilkom UI untuk senantiasa diberkati Tuhan serta dalam perlindungan Roh Kudus baik dalam kegiatan
         KMK maupun aktivitas sehari-hari. Selain itu, sinar yang berasal dari balik Salib melambangkan harapan KMK untuk
-        menjadi terang dunia seperti yang tertulis dalam Kitab Suci.</p>
+        menjadi terang dunia seperti yang tertulis dalam Kitab Suci.
+      </p>
     </div>
   )
 }
