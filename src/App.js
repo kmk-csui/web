42938c875fc2deb8e4d7dad4ca6641c45029b8f9
Teofanus Gary Setiawan <40825899+garyteofanus@users.noreply.github.com>
2021-12-14 03:26:09 +0700

    Tagar (#8)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Filosofi from './components/Filosofi/Filosofi';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
+import Tagar from './components/Tagar/Tagar';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Header />
       <div className="Section">
         <Filosofi />
+        <Tagar />
       </div>
     </div>
   );
---
+++ src/components/Tagar/Tagar.js
@@ -0,0 +1,14 @@
+import Header from "../../components/SectionHeader/SectionHeader";
+import './Tagar.css';
+
+export default function Tagar() {
+    return (
+        <section className="Tagar">
+            <Header color="#CFE7E7">Tagar Kami</Header>
+            <div className="Content">
+                <p className="Latin">#LuceatLuxVestra</p>
+                <p className="Englist">Let Your Light Shine</p>
+            </div>
+        </section>
+    )
+}
