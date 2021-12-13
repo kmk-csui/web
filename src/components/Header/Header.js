ba0d32f42ceb44fb3a6f9d9e44af15469b395e45
Teofanus Gary <garyteofanus@gmail.com>
2021-12-14 00:00:25 +0700

    integrate header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types'
+import LogoKMK from '../../assets/BigLogoKMK.svg'
+import './header.css'
+
+
+export default function Header({ headerTitle }) {
+    return (
+        <header className="header">
+            <img src={LogoKMK} className="logo" alt="Logo KMK Fasilkom" />
+            <h1 className="headerTitle">{headerTitle}</h1>
+            <h2 className="tagline">#LuceatLuxVestra</h2>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    headerTitle: "KMK Fasilkom UI 2021"
+}
+
+Header.propTypes = {
+    headerTitle: PropTypes.string
+}
