ed5df439ec846693110ba7f026738246083eecf8
Teofanus Gary <garyteofanus@gmail.com>
2021-12-14 19:25:10 +0700

    refactor homepage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
+import Navbar from 'components/Navbar/Navbar';
+import HomePage from 'pages/HomePage/HomePage';
 import './App.css';
-import Filosofi from './components/Filosofi/Filosofi';
-import Header from './components/Header/Header';
-import Navbar from './components/Navbar/Navbar';
-import Tagar from './components/Tagar/Tagar';
 
 export default function App() {
   return (
     <div className="App">
       <Navbar />
-      <Header />
-      <div className="Section">
-        <Filosofi />
-        <Tagar />
-      </div>
+      <HomePage />
     </div>
   );
 }
