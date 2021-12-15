11debc6eef610347bebeaa2128ef5a4f177b4e47
Teofanus Gary <garyteofanus@gmail.com>
2021-12-15 10:56:12 +0700

    add footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import './Footer.css';
+
+export default function Footer() {
+    return (
+        <section className="Footer">
+            <p>Copyright © 2021 - KMK Fasilkom UI 2021</p>
+        </section>
+    )
+}
