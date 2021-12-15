1d1495f5092b48f9107a9f7a289eb9adaa8996c3
Teofanus Gary <garyteofanus@gmail.com>
2021-12-15 11:15:11 +0700

    add gallery page

---
+++ src/pages/GalleryPage/GalleryPage.js
@@ -0,0 +1,10 @@
+import Gallery from 'components/Gallery/Gallery';
+import Header from 'components/Header/Header';
+export default function GalleryPage() {
+    return (
+        <>
+            <Header headerTitle='Galeri KMK Fasilkom UI' />
+            <Gallery />
+        </>
+    )
+}
