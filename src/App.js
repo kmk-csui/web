f5f402d312ba98cb44a8aff01b8c43255f572d01
Teofanus Gary <garyteofanus@gmail.com>
2021-12-15 11:15:19 +0700

    integrate gallery page to main page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Footer from 'components/Footer/Footer';
 import Navbar from 'components/Navbar/Navbar';
-import HomePage from 'pages/HomePage/HomePage';
+import GalleryPage from 'pages/GalleryPage/GalleryPage';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <Navbar />
-      <HomePage />
+      {/* <HomePage /> */}
+      <GalleryPage />
       <Footer />
     </div>
   );
