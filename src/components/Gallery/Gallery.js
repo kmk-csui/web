c961397c9417e36f469ca8081ac007ced51425cf
Teofanus Gary Setiawan <40825899+garyteofanus@users.noreply.github.com>
2021-12-15 11:17:57 +0700

    Gallery Page (#11)

---
+++ src/components/Gallery/Gallery.js
@@ -0,0 +1,18 @@
+import kumpulBersama from 'assets/gallery/kumpul-bersama.png';
+import './Gallery.css';
+
+export default function Gallery() {
+    return (
+        <section className="Gallery">
+            <img src={kumpulBersama} alt="Foto Kumpul Bersama" />
+            <img src={kumpulBersama} alt="Foto Kumpul Bersama" />
+            <img src={kumpulBersama} alt="Foto Kumpul Bersama" />
+            <img src={kumpulBersama} alt="Foto Kumpul Bersama" />
+            <img src={kumpulBersama} alt="Foto Kumpul Bersama" />
+            <img src={kumpulBersama} alt="Foto Kumpul Bersama" />
+            <img src={kumpulBersama} alt="Foto Kumpul Bersama" />
+            <img src={kumpulBersama} alt="Foto Kumpul Bersama" />
+            <img src={kumpulBersama} alt="Foto Kumpul Bersama" />
+        </section>
+    )
+}
