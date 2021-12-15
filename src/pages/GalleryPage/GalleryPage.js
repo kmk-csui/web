53fc9e199c95680ed3f7382961316f9ca26fd8c8
Teofanus Gary <garyteofanus@gmail.com>
2021-12-15 11:28:27 +0700

    update styling for gallery

--- src/pages/GalleryPage/GalleryPage.js
+++ src/pages/GalleryPage/GalleryPage.js
@@ -1,10 +1,14 @@
 import Gallery from 'components/Gallery/Gallery';
 import Header from 'components/Header/Header';
+import './GalleryPage.css';
+
 export default function GalleryPage() {
     return (
         <>
             <Header headerTitle='Galeri KMK Fasilkom UI' />
-            <Gallery />
+            <div className='GalleryPage'>
+                <Gallery />
+            </div>
         </>
     )
 }
