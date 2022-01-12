7a975515b0938ba6ee984c90a86acea3e1847e4b
Teofanus Gary Setiawan <garyteofanus@gmail.com>
2022-01-13 04:01:07 +0700

    implement responsive gallery page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,24 @@
 import LogoKMK from "assets/BigLogoKMK.svg";
 import PropTypes from "prop-types";
 import "./Header.css";
+import {Desktop, Mobile} from "../Responsive/Responsive";
 
 export default function Header({headerTitle}) {
   return (
-    <header className="header">
-      <img src={LogoKMK} className="logo" alt="Logo KMK Fasilkom"/>
-      <h1 className="headerTitle">{headerTitle}</h1>
-      <h2 className="tagline">#LuceatLuxVestra</h2>
-    </header>
+    <>
+      <Desktop>
+        <header className="header">
+          <img src={LogoKMK} className="logo" alt="Logo KMK Fasilkom"/>
+          <h1 className="headerTitle">{headerTitle}</h1>
+          <h2 className="tagline">#LuceatLuxVestra</h2>
+        </header>
+      </Desktop>
+      <Mobile>
+        <header className="HeaderMobile">
+          <h1 className="headerTitleMobile">{headerTitle}</h1>
+        </header>
+      </Mobile>
+    </>
   );
 }
 
