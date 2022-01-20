c5a9268286a5ec0abcb1d8468b815c3cccaf0cd4
Teofanus Gary Setiawan <40825899+garyteofanus@users.noreply.github.com>
2022-01-21 02:55:48 +0700

    Update metada (#12)

--- src/pages/GalleryPage/GalleryPage.js
+++ src/pages/GalleryPage/GalleryPage.js
@@ -1,10 +1,14 @@
 import Gallery from "components/Gallery/Gallery";
 import Header from "components/Header/Header";
 import "./GalleryPage.css";
+import {Helmet} from "react-helmet";
 
 export default function GalleryPage() {
   return (
     <>
+      <Helmet>
+        <title>Galeri - KMK CSUI</title>
+      </Helmet>
       <Header headerTitle="Galeri KMK Fasilkom UI"/>
       <div className="GalleryPage">
         <Gallery/>
