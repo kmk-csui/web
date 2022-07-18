ab3eb32e0942425f7c1ceae48126ebaa2f2fd143
Nicholas Sidharta <toxicancm@gmail.com>
2022-07-18 13:10:13 +0700

    Event Page First Push

---
+++ src/components/CardItems/CardItems.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Link} from'react-router-dom'
+import './CardItems.css'
+
+function CardItems(props) {
+  return (
+    <li className='cards__item'>
+        <Link className='cards__item__link' to={props.path}>
+            <figure className='cards__item__pic-wrap' data-category={props.label}>
+                <img
+                    className='cards__item__img'
+                    alt='highlights'
+                    src={props.src}
+                />
+            </figure>
+            <div className='cards__item__info'>
+                <h5 className='cards__item__text'>{props.text}</h5>
+                <div className="cards__item__info__data">
+                    <p className="cards__item__text__data">📅 {props.tanggal}</p>
+                    <p className="cards__item__text__data">🕒 {props.jam}</p>
+                    <p className="cards__item__text__data">📌 {props.lokasi}</p>
+                    <p className="cards__item__text__data">🔗 {props.link}</p>
+                </div>
+                <hr className='break__line'></hr>
+                <p className="cards__item__division">{props.division}</p>
+            </div>
+        </Link>
+    </li>
+  )
+}
+
+export default CardItems
---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import CardItems from 'components/CardItems/CardItems'
+import SectionHeader from 'components/SectionHeader/SectionHeader'
+
+function Cards() {
+  return (
+    <div className='cards'>
+      <div className='cards__container'>
+          <div className='cards__wrapper'>
+            <SectionHeader color="#CFE7E7" isGallerypage={true}>Upcoming Events</SectionHeader>
+            <ul className='cards__items'>
+                <CardItems
+                src='../gallery/kut-2019/1.jpg'
+                text='kut 2019'
+                label='kut'
+                path='/event'
+                tanggal='Friday, July 1'
+                jam='13.00'
+                lokasi='Discord'
+                link='bit.ly/daftar'
+                division='PSDM'
+                />
+                <CardItems
+                src='../gallery/kut-2019/2.jpg'
+                text='kut 2019'
+                label='kut'
+                path='/event'
+                tanggal='Friday, July 1'
+                jam='13.00'
+                lokasi='Discord'
+                link='bit.ly/daftar'
+                division='PSDM'
+                />
+                <CardItems
+                src='../gallery/kut-2019/3.jpg'
+                text='kut 2019'
+                label='kut'
+                path='/event'
+                tanggal='Friday, July 1'
+                jam='13.00'
+                lokasi='Discord'
+                link='bit.ly/daftar'
+                division='PSDM'
+                />
+            </ul>
+
+            <hr className='main__break__line'></hr>
+
+            <SectionHeader color="#CFE7E7" isGallerypage={true}>Previous Events</SectionHeader>
+            <ul className='cards__items'>
+                <CardItems
+                src='../gallery/kut-2019/1.jpg'
+                text='kut 2019'
+                label='kut'
+                path='/event'
+                tanggal='Friday, July 1'
+                jam='13.00'
+                lokasi='Discord'
+                link='bit.ly/daftar'
+                division='PSDM'
+                />
+                <CardItems
+                src='../gallery/kut-2019/2.jpg'
+                text='kut 2019'
+                label='kut'
+                path='/event'
+                tanggal='Friday, July 1'
+                jam='13.00'
+                lokasi='Discord'
+                link='bit.ly/daftar'
+                division='PSDM'
+                />
+                <CardItems
+                src='../gallery/kut-2019/3.jpg'
+                text='kut 2019'
+                label='kut'
+                path='/event'
+                tanggal='Friday, July 1'
+                jam='13.00'
+                lokasi='Discord'
+                link='bit.ly/daftar'
+                division='PSDM'
+                />
+            </ul>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default Cards
