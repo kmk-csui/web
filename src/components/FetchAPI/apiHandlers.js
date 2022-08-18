388a7d23a6274d480445482fc45c32d6f7d0a0d5
Christopher Bagas <70851036+chrisbagas@users.noreply.github.com>
2022-08-19 00:53:17 +0700

    Update apiHandlers.js

--- src/components/FetchAPI/apiHandlers.js
+++ src/components/FetchAPI/apiHandlers.js
@@ -4,7 +4,7 @@ export const fetchWrapper = {
     put,
     delete: _delete
 };
-const BASE_URL = "http://127.0.0.1:8000/"
+const BASE_URL = "https://kmk-csui-backend.herokuapp.com/"
 function get(url) {
     const requestOptions = {
         method: 'GET',
